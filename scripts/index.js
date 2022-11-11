894e875ed22e818d7d965f97b9f68ed7b8be2a69
GuidoNadotti <gpmnadotti@gmail.com>
2022-11-11 09:57:19 -0300

    cambios

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,18 @@
-const USUARIOS = "https://6364250f8a3337d9a2f256de.mockapi.io/users/"
-let input = document.getElementById("inputGet1Id")
+const USUARIOS = "https://6364250f8a3337d9a2f256de.mockapi.io/users/" + ""
+const BOTON_BUSCAR=document.getElementById("btnGet1")
+const BOTON_AGREGAR=document.getElementById("btnPost")
+const BOTON_MODIFICAR=document.getElementById("btnSendChanges")
+const BOTON_BORRAR=document.getElementById("btnDelete")
 
-function buscar(datos) {
+const INPUTS_INGRESAR = document.getElementById("post-box").querySelectorAll("input[type=text]")
+const INPUT_BUSCAR = document.getElementById("inputGet1Id")
+const INPUT_MODIFICAR = document.getElementById("inputPutId")
+const INPUT_BORRAR=document.getElementById("inputDelete")
+const INPUTS_MODAL = document.getElementById("dataModal").querySelectorAll("input[type=text]")
+
+function mostrarLista(datos) {
     document.getElementById("results").innerHTML = ""
-    if (input.value == "") {
+    if (INPUT_BUSCAR.value == "") {
         for (let usuario of datos) {
             document.getElementById("results").innerHTML +=
                 `
@@ -21,61 +30,146 @@ function buscar(datos) {
         <li>lastname: ${datos.lastname}</li>
         `
     }
-
-
 }
 
-
+let showSpinner = function () {
+    document.getElementById("spinner-wrapper").style.display = "block";
+}
+let hideSpinner = function () {
+    document.getElementById("spinner-wrapper").style.display = "none";
+}
+let mostrarAlerta= function(){
+    swal("Algo salió mal", "¡Te mandaste cualquiera!", "error", { button: "😢", });
+}
 
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("btnGet1").addEventListener("click", function () {
-        fetch(USUARIOS + input.value)
-            .then(response => response.json())
-            .then(data => buscar(data)
-            )
+    // Funciones para el botón buscar.
+    BOTON_BUSCAR.addEventListener("click", function () {
+        showSpinner()
+        fetch(USUARIOS + INPUT_BUSCAR.value)
+            .then(response => {
+                if (response.ok) {
+                    console.log(response);
+                    return response.json()
+                } else {
+                    mostrarAlerta();
+                    hideSpinner()
+                }
+            })
+            .then((data) => {
+                mostrarLista(data)
+                hideSpinner()
+            })
     })
-
-    let inputsIngresar = document.getElementById("post-box").querySelectorAll("input[type=text]")
-    inputsIngresar.forEach(input => {
+    
+    // Habilitar o deshabilitar el botón ingresar.
+    INPUTS_INGRESAR.forEach(input => {
         input.addEventListener("input", function () {
-            document.getElementById("btnPost").disabled = !(inputsIngresar[0].value != "" && inputsIngresar[1].value != "")
+            BOTON_AGREGAR.disabled = !(INPUTS_INGRESAR[0].value != "" && INPUTS_INGRESAR[1].value != "")
         })
     })
 
-    document.getElementById("btnPost").addEventListener("click",function(){
+    // Funciones para el botón agregar.
+    BOTON_AGREGAR.addEventListener("click", function () {
+        showSpinner()
         fetch(USUARIOS,
-            {method:"POST",
-            headers: { 'Content-Type': 'application/json'},
-            body:JSON.stringify({
-                name:inputsIngresar[0].value,
-                lastname:inputsIngresar[0].value
-                })          
+            {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    name: INPUTS_INGRESAR[0].value,
+                    lastname: INPUTS_INGRESAR[1].value
+                })
             }
-        )
-    })
-
-    let inputModificar=document.getElementById("inputPutId")
-    inputModificar.addEventListener("input",function(){
-        document.getElementById("btnPut").disabled=inputModificar.value==""
-    })
-
-    let inputsModal = document.getElementById("dataModal").querySelectorAll("input[type=text]")
-    inputsModal.forEach(input => {
-        input.addEventListener("input", function () {
-            document.getElementById("btnSendChanges").disabled = !(inputsModal[0].value != "" && inputsModal[1].value != "")
+        ).then(function(data){
+            if (data.ok) {
+                fetch(USUARIOS)
+                    .then(response => response.json())
+                    .then(data => mostrarLista(data))
+                hideSpinner()
+            }else{
+                mostrarAlerta();
+                hideSpinner()
+            }
+        }
+        ).catch(function (error) {
+            mostrarAlerta();
+            hideSpinner()
         })
     })
 
-    document.getElementById("btnSendChanges").addEventListener("click",function(){
-        let idAModificar=document.getElementById("inputPutId").value
-        fetch(USUARIOS+idAModificar,
-            {method:"PUT",
-            headers: { 'Content-Type': 'application/json'},
-            body:JSON.stringify({
-                name:inputsModal[0].value,
-                lastname:inputsModal[1].value
-                })          
+    // Habilitar o deshabilitar el botón modificar.
+    INPUT_MODIFICAR.addEventListener("input", function () {
+        document.getElementById("btnPut").disabled = INPUT_MODIFICAR.value == ""
+    })
+
+    // Habilitar o deshabilitar el botón guardar (dentro del modal).
+    INPUTS_MODAL.forEach(input => {
+        input.addEventListener("input", function () {
+            BOTON_MODIFICAR.disabled = !(INPUTS_MODAL[0].value != "" && INPUTS_MODAL[1].value != "")
+        })
+    })
+
+    // Funciones para el botón modificar.
+    BOTON_MODIFICAR.addEventListener("click", function () {
+        showSpinner()
+        let idAModificar = document.getElementById("inputPutId").value
+        fetch(USUARIOS + idAModificar,
+            {
+                method: "PUT",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    name: INPUTS_MODAL[0].value,
+                    lastname: INPUTS_MODAL[1].value
+                })
             }
-        )
+        ).then(function (data) {
+            console.log(data);
+            if (data.ok) {
+                fetch(USUARIOS)
+                    .then(response => response.json())
+                    .then(data => mostrarLista(data))
+                hideSpinner()
+            }else{
+                mostrarAlerta();
+                hideSpinner()  
+            }
+        })
+            .catch(function (error) {
+                mostrarAlerta();
+                hideSpinner()
+            })
+    })
+
+    // Habilitar o deshabilitar el botón eliminar.
+    INPUT_BORRAR.addEventListener("input", function () {
+        BOTON_BORRAR.disabled = this.value == ""
+    })
+
+    //Funciones para el botón borrar. 
+    BOTON_BORRAR.addEventListener("click", function () {
+        showSpinner()
+        let idAEliminar = INPUT_BORRAR.value
+        fetch(USUARIOS + idAEliminar,
+            {
+                method: "DELETE",
+                headers: { 'Content-Type': 'application/json' },
+            }
+        ).then(function (data) {
+            if (data.ok) {
+                fetch(USUARIOS)
+                    .then(response => response.json())
+                    .then(data => mostrarLista(data))
+                hideSpinner()
+            } else {
+                mostrarAlerta();
+                hideSpinner()
+            }
+        }).catch(function (error) {
+            mostrarAlerta();
+            hideSpinner()
+        })
+
+
     })
 })
