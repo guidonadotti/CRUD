c844b376bc048616a34589c76945aba4811b9bb5
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2022-11-13 18:23:02 -0300

    Mostrar los datos al abrir el modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
 const USUARIOS = "https://6364250f8a3337d9a2f256de.mockapi.io/users/";
 const BOTON_BUSCAR = document.getElementById("btnGet1");
 const BOTON_AGREGAR = document.getElementById("btnPost");
-const BOTON_MODIFICAR = document.getElementById("btnSendChanges");
+const BOTON_GUARDAR = document.getElementById("btnSendChanges");
+const BOTON_MODIFICAR = document.getElementById("btnPut"); 
 const BOTON_BORRAR = document.getElementById("btnDelete");
 
 const INPUT_BUSCAR = document.getElementById("inputGet1Id");
@@ -9,25 +10,28 @@ const INPUT_MODIFICAR = document.getElementById("inputPutId");
 const INPUT_BORRAR = document.getElementById("inputDelete");
 const INPUTS_INGRESAR = document.getElementById("post-box").querySelectorAll("input[type=text]");
 const INPUTS_MODAL = document.getElementById("dataModal").querySelectorAll("input[type=text]");
+const MODAL_NOMBRE = document.getElementById("inputPutNombre");
+const MODAL_APELLIDO = document.getElementById("inputPutApellido");
+const MODAL = document.getElementById("dataModal");
 
 // Función que muestra la lista de usuarios en el HTML.
 function mostrarLista(datos) {
-    document.getElementById("results").innerHTML = ""
-    if (INPUT_BUSCAR.value == "") {
+    document.getElementById("results").innerHTML = "";
+    if (datos.length > 0) {
         for (let usuario of datos) {
             document.getElementById("results").innerHTML += `
             <li>ID: ${usuario.id}</li>
             <li>name: ${usuario.name}</li>
             <li>lastname: ${usuario.lastname}</li>
             <hr>
-            `
+            `;
         }
     } else {
         document.getElementById("results").innerHTML = `
         <li>ID: ${datos.id}</li>
         <li>name: ${datos.name}</li>
         <li>lastname: ${datos.lastname}</li>
-        `
+        `;
     }
 }
 
@@ -52,7 +56,7 @@ document.addEventListener("DOMContentLoaded", function () {
         fetch(USUARIOS + INPUT_BUSCAR.value)
             .then(response => {
                 if (response.ok) {
-                    return response.json()
+                    return response.json();
                 } else {
                     mostrarAlerta();
                     hideSpinner();
@@ -67,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Habilitar o deshabilitar el botón ingresar.
     INPUTS_INGRESAR.forEach(input => {
         input.addEventListener("input", function () {
-            BOTON_AGREGAR.disabled = !(INPUTS_INGRESAR[0].value != "" && INPUTS_INGRESAR[1].value != "")
+            BOTON_AGREGAR.disabled = !(INPUTS_INGRESAR[0].value != "" && INPUTS_INGRESAR[1].value != "");
         })
     })
 
@@ -88,7 +92,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 fetch(USUARIOS)
                     .then(response => response.json())
                     .then(data => mostrarLista(data))
-                hideSpinner()
+                hideSpinner();
             } else {
                 mostrarAlerta();
                 hideSpinner();
@@ -105,17 +109,32 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("btnPut").disabled = INPUT_MODIFICAR.value == "";
     })
 
+    //Obtener los valores del recurso almacenado con ese ID para mostrarlos en los campos del modal (si existe).
+    BOTON_MODIFICAR.addEventListener("click", function() {
+        fetch(USUARIOS + INPUT_MODIFICAR.value).then(data => {
+            if (data.ok){
+                return data.json();
+            } else {
+                mostrarAlerta();
+                MODAL_NOMBRE.value = "";
+                MODAL_APELLIDO.value = "";
+            }
+        }).then (data => {
+            MODAL_NOMBRE.value = data.name;
+            MODAL_APELLIDO.value = data.lastname;
+        })
+    })
     // Habilitar o deshabilitar el botón guardar (dentro del modal).
     INPUTS_MODAL.forEach(input => {
         input.addEventListener("input", function () {
-            BOTON_MODIFICAR.disabled = !(INPUTS_MODAL[0].value != "" && INPUTS_MODAL[1].value != "")
+            BOTON_GUARDAR.disabled = !(INPUTS_MODAL[0].value != "" && INPUTS_MODAL[1].value != "");
         })
     })
 
     // Funciones para el botón modificar.
-    BOTON_MODIFICAR.addEventListener("click", function () {
+    BOTON_GUARDAR.addEventListener("click", function () {
         showSpinner();
-        let idAModificar = document.getElementById("inputPutId").value
+        let idAModificar = document.getElementById("inputPutId").value;
         fetch(USUARIOS + idAModificar,
             {
                 method: "PUT",
